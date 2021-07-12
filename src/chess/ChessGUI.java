f0016d19d161fdb67f84c79aeecc8f7c604ecbd7
emilydeppe <emilydeppe@172.10.9.191>
2021-07-12 12:00:08 -0400

    Added constants, a location object and a moveable implementation

--- src/chess/ChessGUI.java
+++ src/chess/ChessGUI.java
@@ -2,14 +2,57 @@ package chess;
 
 import javax.swing.*;
 import java.util.*;
-import java.awt.*; 
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener; 
 
-public class ChessGUI {
-	public static void main(String arg[]){
-
-		JFrame frame = new JFrame("The Game of Chess");
-		JPanel pane = new JPanel(new GridBagLayout());
-		JPanel board= new JPanel(new GridLayout(8,8));
+public class ChessGUI extends JFrame implements ActionListener, MouseListener{
+	private String rules = "The ultimate aim in the chess game is delivering a checkmate – trapping your opponent´s king.\n"
+						 + "White is always first to move and players take turns alternately moving one piece at a time.\n"
+						 + "Movement is required. If a player´s turn is to move, he is not in check but has no legal moves,\n"
+						 + "this situation is called “Stalemate” and it ends the game in a draw. Each type of piece has\n"
+						 + "its own method of movement. A piece may be moved to another position or may capture an opponent´s\n"
+						 + "piece, replacing on its square (en passant being the only exception). With the exception of the\n"
+						 + "knight, a piece may not move over or through any of the other pieces. When a king is threatened\n"
+						 + "with capture (but can protect himself or escape), it´s called check. If a king is in check, then\n"
+						 + "the player must make a move that eliminates the threat of capture and cannot leave the king in check.\n"
+						 + "Checkmate happens when a king is placed in check and there is no legal move to escape. Checkmate ends\n"
+						 + "the game and the side whose king was checkmated looses.\n"
+						 + "King can move exactly one square horizontally, vertically, or diagonally.\n"
+						 + "Queen can move any number of vacant squares diagonally, horizontally, or vertically.\n"
+						 + "Rook can move any number of vacant squares vertically or horizontally. It also is moved while castling.\n"
+						 + "Bishop can move any number of vacant squares in any diagonal direction.\n"
+						 + "Knight can move one square along any rank or file and then at an angle.\n"
+						 + "The knight´s movement can also be viewed as an “L” or “7″ laid out at any horizontal or vertical angle.\n"
+						 + "Pawns can move forward one square, if that square is unoccupied. If it has not yet moved, the pawn has the\n"
+						 + "option of moving two squares forward provided both squares in front of the pawn are unoccupied. A pawn\n"
+						 + "cannot move backward. Pawns are the only pieces that capture differently from how they move. They can capture\n"
+						 + "an enemy piece on either of the two spaces adjacent to the space in front of them (i.e., the two squares\n"
+						 + "diagonally in front of them) but cannot move to these spaces if they are vacant. ";
+	
+	JMenuBar menus;
+    JMenu fileMenu;
+    JMenuItem quitItem;
+    JMenuItem rulesItem;
+    
+	JPanel pane = new JPanel(new GridBagLayout());
+	JPanel board= new JPanel(new GridLayout(8,8));
+	
+    
+    public static void main(String args[]){
+    	ChessGUI gui = new ChessGUI();
+    	gui.pane.add(gui.board);
+		gui.add(gui.pane);
+		gui.setSize(1000, 1000);
+		gui.setTitle("The Game of Chess");
+		gui.getContentPane().setPreferredSize(new Dimension(600,600));
+		gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		gui.setVisible(true);
+	}
+    
+	public ChessGUI() {
 		
 		int n=0;int a=1;int b=0;
 		
@@ -27,6 +70,7 @@ public class ChessGUI {
 				a=b;
 				b=temp;				
 			}			
+			label.addMouseListener(this);
 			board.add(label, n);
 			n++;
 		}
@@ -97,13 +141,69 @@ public class ChessGUI {
 		((JPanel) board.getComponent(48)).remove(0);
 		((JPanel) board.getComponent(48)).add(new JLabel(new ImageIcon("pawnW.png")));
 		
-		
-		
-		pane.add(board);
-		frame.add(pane);
-		frame.setSize(1000, 1000);
-		frame.getContentPane().setPreferredSize(new Dimension(600,600));
-		frame.setVisible(true);
+		setupMenus();
+	}
+	private void setupMenus(){
 
+        // create quit menu item
+        quitItem = new JMenuItem("Quit");
+        quitItem.addActionListener(this);
+
+        // create open... item menu
+        rulesItem = new JMenuItem("Rules");
+        rulesItem.addActionListener(this);
+        
+        // add items to menu
+        fileMenu = new JMenu("File");
+        fileMenu.add(rulesItem);
+        fileMenu.add(quitItem);
+
+        // display menu bar and File menu
+        menus = new JMenuBar();
+        menus.add(fileMenu);  
+        setJMenuBar(menus);
+    }
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+        // user selects open...
+        if (e.getSource() == rulesItem){
+        	JOptionPane.showMessageDialog(this, rules);
+        } 
+        // user selects quit
+        if (e.getSource() == quitItem){
+            System.exit(1);
+        }
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		((JPanel) e.getComponent()).setBackground(Color.blue);
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
--- src/chess/Piece.java
+++ src/chess/Piece.java
@@ -1,5 +1,50 @@
 package chess;
 
-public class Piece {
-
+public interface Piece {
+	/*
+	 * check to see if the move is valid
+	 * */
+	boolean validateMove(Location destination);
+	
+	/*
+	 * makes the move and returns true if success , otherwise false
+	 * */
+	boolean makeMove(Location destination);
+	
+	/*
+	 *	Checks to see if the opposing king is checked 
+	 * */
+	boolean hasCheckOnOpossingKing(Location positionOfOpposingKing);
+	
+	/*
+	 *	Active or not 
+	 * */
+	boolean isActive();
+	
+	/*
+	 *	Black or not 
+	 * */
+	boolean isBlack();
+	
+	/*
+	 *	White or not 
+	 * */
+	boolean isWhite();
+	
+	/*
+	 * 
+	 * */
+	Piece kill(Piece a);
+	
+	/*
+	 * 
+	 */
+	
+	public Location getLocation();
+	
+	public int getColor();
+	
+	public void setPosition(Location l);
+	
+	public void setState(boolean a);
 }
