2baec5e08dc6f9fc0a1e1a3b0303b6b5c41fd08b
Chris Kyser <kyserch@mail.gvsu.edu>
2021-07-26 13:09:12 -0400

    Storing

--- src/chess/ChessGUI.java
+++ src/chess/ChessGUI.java
@@ -443,14 +443,14 @@ ActionListener, MouseListener {
     }
     
     private void resetBoard() {
-//		ChessGUI gui = new ChessGUI();
-//    	gui.pane.add(gui.board);
-//		gui.add(gui.pane);
-//		gui.setSize(1000, 1000);
-//		gui.setTitle("The Game of Chess");
-//		gui.getContentPane().setPreferredSize(new Dimension(600,600));
-//		gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//		gui.setVisible(true);
+		ChessGUI gui = new ChessGUI();
+    	gui.pane.add(gui.board);
+		gui.add(gui.pane);
+		gui.setSize(1000, 1000);
+		gui.setTitle("The Game of Chess");
+		gui.getContentPane().setPreferredSize(new Dimension(600,600));
+		gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		gui.setVisible(true);
 		Board.getBoard().resetBoard();
 	}
 
